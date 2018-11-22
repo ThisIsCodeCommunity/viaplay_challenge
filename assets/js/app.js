56aaaf8074845d76bdaab81811772c2529e21867
Thomas <thomas@craftacademy.se>
2018-11-22 10:01:32 +0100

    extracts fetch to separate method for better readability

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,53 @@
+const proxyURL = 'https://cors-anywhere.herokuapp.com/'
+const apiUrl = 'https://content.viaplay.se/pc-se/serier/samtliga'
+
+const fetchShows = async () => {
+    let data = await (await fetch(proxyURL + apiUrl)).json();
+    return data;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let displaySection = document.getElementById('main_content')
-    const proxyURL = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'https://content.viaplay.se/pc-se/serier/samtliga'
+    fetchShows().then((data) => {
+        let shows = data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']
+        shows.forEach(show => {
+            console.log(show)
+            let showDiv = document.createElement('div')
+            showDiv.className += 'display-show'
+            let html = ''
+            if (show.content.imdb) {
+                html += `<a href="${show.content.imdb.url}" target="_blank">`
+                html += `<img src="${show.content.images.landscape.url}" >`
+                html += `</a>`
+            } else {
+                html += `<img src="${show.content.images.landscape.url}">`
+            }
 
-    fetch(proxyURL + apiUrl).then(response => {
-        response.json().then(seriesResponse => {
-            let shows = seriesResponse._embedded['viaplay:blocks'][0]._embedded['viaplay:products']
-            shows.forEach(show => {
-                // console.log(show.content.imdb.url)
-                let showDiv = document.createElement('div')
-                showDiv.className += 'display-show'
-                let html = ''
-                if (show.content.imdb) {
-                    html +=`<a href="${show.content.imdb.url}" target="_blank">`
-                    html += `<img src="${show.content.images.landscape.url}" >`
-                    html += `</a>`
-                } else {
-                    html += `<img src="${show.content.images.landscape.url}">`
-                }
-                
-                showDiv.innerHTML = html
-                displaySection.appendChild(showDiv)
-            })
+            showDiv.innerHTML = html
+            displaySection.appendChild(showDiv)
         })
     })
+
+
+    // fetch(proxyURL + apiUrl).then(response => {
+    //     response.json().then(seriesResponse => {
+    //         let shows = seriesResponse._embedded['viaplay:blocks'][0]._embedded['viaplay:products']
+    //         shows.forEach(show => {
+    //             // console.log(show.content.imdb.url)
+    //             let showDiv = document.createElement('div')
+    //             showDiv.className += 'display-show'
+    //             let html = ''
+    //             if (show.content.imdb) {
+    //                 html +=`<a href="${show.content.imdb.url}" target="_blank">`
+    //                 html += `<img src="${show.content.images.landscape.url}" >`
+    //                 html += `</a>`
+    //             } else {
+    //                 html += `<img src="${show.content.images.landscape.url}">`
+    //             }
+
+    //             showDiv.innerHTML = html
+    //             displaySection.appendChild(showDiv)
+    //         })
+    //     })
+    // })
 })
